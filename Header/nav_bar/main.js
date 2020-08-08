607f43eaec8b73325c80dff36b2de97aa7c1f0df
Chaeheyhyun <cogpgus@naver.com>
2020-08-08 14:37:08 +0900

    nav_bar complete

--- Header/nav_bar/main.js
+++ Header/nav_bar/main.js
@@ -1,8 +1,8 @@
-const toggleBtn=document.querySelector('.navbar__toggleBtn');
-const menu=document.querySelector('.navbar__menu');
-const icons=document.querySelector('navbar__icons');
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const menu = document.querySelector('.navbar__menu');
+const icons = document.querySelector('.navbar__icons');
 
-toogleBtn.addEventListner('click', () => {
+toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
-}
+});
